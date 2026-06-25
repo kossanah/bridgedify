343f0878add122e445a2294ef5d1a53e16f327b0
Kaosiso Anaekwe <kossanah@gmail.com>
2026-06-25 20:45:04 +0100

    style: rename to Bridge NG and apply branding updates

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -1,16 +1,14 @@
 import type { Viewport } from '@/next'
-import { ToastHost } from '@langgenius/dify-ui/toast'
-import { TooltipProvider } from '@langgenius/dify-ui/tooltip'
 import { Provider as JotaiProvider } from 'jotai/react'
 import { ThemeProvider } from 'next-themes'
 import { NuqsAdapter } from 'nuqs/adapters/next/app'
-import { IS_PROD } from '@/config'
+import GlobalPublicStoreProvider from '@/context/global-public-context'
 import { TanstackQueryInitializer } from '@/context/query-client'
 import { getDatasetMap } from '@/env'
 import { getLocaleOnServer } from '@/i18n-config/server'
-import { headers } from '@/next/headers'
+import { ToastHost } from './components/base/ui/toast'
+import { TooltipProvider } from './components/base/ui/tooltip'
 import PartnerStackCookieRecorder from './components/billing/partner-stack/cookie-recorder'
-import { CreateAppAttributionBootstrap } from './components/create-app-attribution-bootstrap'
 import { AgentationLoader } from './components/devtools/agentation-loader'
 import { ReactScanLoader } from './components/devtools/react-scan/loader'
 import { I18nServerProvider } from './components/provider/i18n-server'
@@ -21,7 +19,9 @@ import './styles/markdown.css'
 export const viewport: Viewport = {
   width: 'device-width',
   initialScale: 1,
+  maximumScale: 1,
   viewportFit: 'cover',
+  userScalable: false,
 }
 
 const LocaleLayout = async ({
@@ -31,7 +31,6 @@ const LocaleLayout = async ({
 }) => {
   const locale = await getLocaleOnServer()
   const datasetMap = getDatasetMap()
-  const nonce = IS_PROD ? (await headers()).get('x-nonce') ?? undefined : undefined
 
   return (
     <html lang={locale ?? 'en'} className="h-full" suppressHydrationWarning>
@@ -41,18 +40,18 @@ const LocaleLayout = async ({
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
-        <meta name="apple-mobile-web-app-title" content="Dify" />
+        <meta name="apple-mobile-web-app-title" content="Bridge NG" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/icon-192x192.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/icon-192x192.png" />
         <meta name="msapplication-TileColor" content="#1C64F2" />
         <meta name="msapplication-config" content="/browserconfig.xml" />
 
-        <CreateAppAttributionBootstrap />
+        {/* <ReactGrabLoader /> */}
         <ReactScanLoader />
       </head>
       <body
-        className="h-full bg-background-body"
+        className="h-full select-auto"
         {...datasetMap}
       >
         <div className="isolate h-full">
@@ -62,16 +61,18 @@ const LocaleLayout = async ({
               defaultTheme="system"
               enableSystem
               disableTransitionOnChange
-              nonce={nonce}
+              enableColorScheme={false}
             >
               <NuqsAdapter>
                 <TanstackQueryInitializer>
                   <I18nServerProvider>
                     <ToastHost timeout={5000} limit={3} />
                     <PartnerStackCookieRecorder />
-                    <TooltipProvider delay={300} closeDelay={200}>
-                      {children}
-                    </TooltipProvider>
+                    <GlobalPublicStoreProvider>
+                      <TooltipProvider delay={300} closeDelay={200}>
+                        {children}
+                      </TooltipProvider>
+                    </GlobalPublicStoreProvider>
                   </I18nServerProvider>
                 </TanstackQueryInitializer>
               </NuqsAdapter>
